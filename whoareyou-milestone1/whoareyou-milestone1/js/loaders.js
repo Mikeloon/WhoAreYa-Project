6d9cb710fcea8ef7f129fd50f52d835092173aba
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2022-11-12 11:54:07 +0100

    Delete whoareyou-milestone1/whoareyou-milestone1 directory

--- whoareyou-milestone1/whoareyou-milestone1/js/loaders.js
+++
@@ -1,9 +0,0 @@
-export { fetchJSON };
-
-async function fetchJSON(file) {
-    let jsonObj = {};
-    await fetch(file).then(r => r.json()).then(r =>{
-        jsonObj = r
-    });
-    return jsonObj;
-}
